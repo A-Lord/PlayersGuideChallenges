433d02d8276ef94ae8536df4134f916555d471d6
Adam Lord Eriksson <adam.lorderiksson@studerande-skys.se>
2021-10-20 16:28:36 +0200

    Warpreparations done

---
+++ War Preparations/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+Sword a = new Sword(GemStone.Amber,Material.Bronze,30,10);
+Sword b = new Sword(GemStone.Empty, Material.Iron, 33, 15);
+Sword c = new Sword(GemStone.Amber, Material.Bronze, 30, 10);
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+
+
+public record Sword(GemStone gem,Material material,int length,int xGuardWidth);
+public enum Material { Wood, Bronze, Iron, Steel, Binarium }
+public enum GemStone { Empty, Emerald, Amber, Sapphire, Diamond, Bitstone, }
