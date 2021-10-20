ae1743d8d14b577594f97bf406009bda12648306
Adam Lord Eriksson <adam.lorderiksson@studerande-skys.se>
2021-10-20 15:56:39 +0200

    Room Coordinates

---
+++ Room Coordinates/CheckCords.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Room_Coordinates
+{
+    public class CheckCords
+    {
+        public bool IsItClose(List<Coordinate> listOfCords)
+        {
+            foreach (Coordinate c in listOfCords)
+            {
+                for (int i = 0; i < listOfCords.Count; i++)
+                {
+                    if (c.x == listOfCords[i].x || c.x + 1 == listOfCords[i].x || c.x - 1 == listOfCords[i].x || c.y == listOfCords[i].x || c.y + 1 == listOfCords[i].y || c.y - 1 == listOfCords[i].y)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Room Coordinates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using Room_Coordinates;
+
+List<Coordinate> listOfCords = new List<Coordinate>();
+listOfCords.Add(new Coordinate(1, 1));
+listOfCords.Add(new Coordinate(4, 7));
+listOfCords.Add(new Coordinate(1, 2));
+
+CheckCords test = new CheckCords();
+if(test.IsItClose(listOfCords))
+    Console.WriteLine("yeey");
+
+public struct Coordinate
+{
+    public int x { get; }
+    public int y { get; }
+
+    public Coordinate(int X,int Y)
+    {
+        x = X;
+        y = Y;  
+    }
+}
